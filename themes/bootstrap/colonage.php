515d8b44afa968fa5bd3fca15c428246ce76df56
GhassaneCHAHOUD <ghassane159@gmail.com>
2013-01-28 22:45:15 +0100

    travail final

--- themes/bootstrap/colonage.php
+++ themes/bootstrap/colonage.php
@@ -8,25 +8,37 @@
 		    <div class="span4">
 		        
 				<div class="hero-unit">
-				
+				 <?php
+				 $a = new Area('col 1');
+				  $a->display($c);
+				  ?>
 				</div>
 		
 			</div>
 			 <div class="span4">
 		       <div class="hero-unit">
-				
+				 <?php
+				 $a = new Area('col 2');
+				  $a->display($c);
+				  ?>
 				</div>
 			</div>
 			 <div class="span4">
 		        <div class="hero-unit">
-				 
+				 <?php
+				 $a = new Area('col 3');
+				  $a->display($c);
+				  ?>
 				</div>
 			</div>
 		</div>
 		<div class="row">
 		    <div class="span12">
 		       <div class="hero-unit">
-				 
+				 <?php
+				 $a = new Area('bas 12');
+				  $a->display($c);
+				  ?>
 				</div>
 			</div>
 			 
